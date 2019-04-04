4db3054ca8af26847536d6b93810900d0e3106b0
makubass <makubass@gmail.com>
2019-04-05 00:45:26 +0900

    Filename Changed.

--- 03_VNETconfig.ps1
+++
@@ -1,9 +0,0 @@
-$rgName="AzureTestLab01"
-$locName=(Get-AzResourceGroup -Name $rgName).Location
-$corpnetSubnet=New-AzVirtualNetworkSubnetConfig -Name Corpnet -AddressPrefix 10.0.0.0/24
-New-AzVirtualNetwork -Name TestLab -ResourceGroupName $rgName -Location $locName -AddressPrefix 10.0.0.0/8 -Subnet $corpnetSubnet -DNSServer 10.0.0.4
-$rule1=New-AzNetworkSecurityRuleConfig -Name "RDPTraffic" -Description "Allow RDP to all VMs on the subnet" -Access Allow -Protocol Tcp -Direction Inbound -Priority 100 -SourceAddressPrefix Internet -SourcePortRange * -DestinationAddressPrefix * -DestinationPortRange 3389
-New-AzNetworkSecurityGroup -Name Corpnet -ResourceGroupName $rgName -Location $locName -SecurityRules $rule1
-$vnet=Get-AzVirtualNetwork -ResourceGroupName $rgName -Name TestLab
-$nsg=Get-AzNetworkSecurityGroup -Name Corpnet -ResourceGroupName $rgName
-Set-AzVirtualNetworkSubnetConfig -VirtualNetwork $vnet -Name Corpnet -AddressPrefix "10.0.0.0/24" -NetworkSecurityGroup $nsg
