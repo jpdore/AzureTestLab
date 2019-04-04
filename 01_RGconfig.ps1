2edea0c8142ccdac62d1e6aad4a40d71fd7c946d
makubass <makubass@gmail.com>
2019-04-05 00:02:08 +0900

    Add commnet for subuscription name.

--- 01_RGconfig.ps1
+++ 01_RGconfig.ps1
@@ -2,6 +2,7 @@ Connect-AzAccount
 
 Get-AzSubscription | Sort Name | Select Name
 
+## 先にAzure上でサブスクリプション名を"無料試用版"から"Free Trial"に変更しておく
 $subscrName="Free Trial"
 Select-AzSubscription -SubscriptionName $subscrName
 
