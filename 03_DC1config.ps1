97135d85f2227db52f4581d4a10cacbffcfd96be
makubass <makubass@gmail.com>
2019-04-05 23:58:25 +0900

    New-AzStorageAccount added.

--- 03_DC1config.ps1
+++ 03_DC1config.ps1
@@ -8,8 +8,9 @@ $cred=Get-Credential -Message "Type the name and password of the local administr
 $vm=Set-AzVMOperatingSystem -VM $vm -Windows -ComputerName DC1 -Credential $cred -ProvisionVMAgent -EnableAutoUpdate
 $vm=Set-AzVMSourceImage -VM $vm -PublisherName MicrosoftWindowsServer -Offer WindowsServer -Skus 2016-Datacenter -Version "latest"
 $vm=Add-AzVMNetworkInterface -VM $vm -Id $nic.Id
+$storageAccount = New-AzStorageAccount -ResourceGroupName $rgName -AccountName azuretestlab01 -Location westus -SkuName Standard_LRS #事前にStorageAccount作成
 $vm=Set-AzVMOSDisk -VM $vm -Name "DC1-OS" -DiskSizeInGB 128 -CreateOption FromImage -StorageAccountType "Standard_LRS"
-$diskConfig=New-AzDiskConfig -AccountType "Standard_LRS" -Location $locName -CreateOption Empty -DiskSizeGB 20
+$diskConfig=New-AzDiskConfig -AccountType "Standard_LRS" -Location $locName -CreateOption Empty -DiskSizeGB 20 -StorageAccountId $storageAccount
 $dataDisk1=New-AzDisk -DiskName "DC1-DataDisk1" -Disk $diskConfig -ResourceGroupName $rgName
 $vm=Add-AzVMDataDisk -VM $vm -Name "DC1-DataDisk1" -CreateOption Attach -ManagedDiskId $dataDisk1.Id -Lun 1
 New-AzVM -ResourceGroupName $rgName -Location $locName -VM $vm
