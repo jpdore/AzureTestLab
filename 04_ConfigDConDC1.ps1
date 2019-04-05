2dfa1a777da8dc0505c57a66ecbe46a714028aa5
makubass <makubass@gmail.com>
2019-04-06 00:14:07 +0900

    Install ADDS added.

---
+++ 04_ConfigDConDC1.ps1
@@ -0,0 +1,3 @@
+Get-Disk | Where PartitionStyle -eq "RAW" | Initialize-Disk -PartitionStyle MBR -PassThru | New-Partition -AssignDriveLetter -UseMaximumSize | Format-Volume -FileSystem NTFS -NewFileSystemLabel "WSAD Data"
+Install-WindowsFeature AD-Domain-Services -IncludeManagementTools
+Install-ADDSForest -DomainName corp.contoso.com -DatabasePath "F:\NTDS" -SysvolPath "F:\SYSVOL" -LogPath "F:\Logs"
