be8f7c06bf2344d3697ccdf09cb6e321a23b14bb
makubass <makubass@gmail.com>
2019-04-06 02:18:51 +0900

    Initial Commit.

---
+++ 09_Start&Stop-VM.ps1
@@ -0,0 +1,12 @@
+## VMの起動
+$rgName="AzureTestLab01"
+Start-AzVM -ResourceGroupName $rgName -Name "DC1"
+Start-AzVM -ResourceGroupName $rgName -Name "APP1"
+Start-AzVM -ResourceGroupName $rgName -Name "CLIENT1"
+
+
+## VMの停止
+$rgName="AzureTestLab01"
+Stop-AzVM -ResourceGroupName $rgName -Name "CLIENT1" -Force
+Stop-AzVM -ResourceGroupName $rgName -Name "APP1" -Force
+Stop-AzVM -ResourceGroupName $rgName -Name "DC1" -Force
