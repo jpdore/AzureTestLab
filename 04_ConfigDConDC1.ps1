9d986e1816024a46b9db6c3deebeef4c53291934
makubass <makubass@gmail.com>
2019-04-06 00:35:06 +0900

    Create User1 added.

--- 04_ConfigDConDC1.ps1
+++ 04_ConfigDConDC1.ps1
@@ -1,3 +1,14 @@
+## DC1\localadmin で実行
 Get-Disk | Where PartitionStyle -eq "RAW" | Initialize-Disk -PartitionStyle MBR -PassThru | New-Partition -AssignDriveLetter -UseMaximumSize | Format-Volume -FileSystem NTFS -NewFileSystemLabel "WSAD Data"
 Install-WindowsFeature AD-Domain-Services -IncludeManagementTools
 Install-ADDSForest -DomainName corp.contoso.com -DatabasePath "F:\NTDS" -SysvolPath "F:\SYSVOL" -LogPath "F:\Logs"
+## 自動再起動
+
+## RDP再接続
+## corp\localadmin で実行
+New-ADUser -SamAccountName User1 -AccountPassword (read-host "Set user password" -assecurestring) -name "User1" -enabled $true -PasswordNeverExpires $true -ChangePasswordAtLogon $false
+Add-ADPrincipalGroupMembership -Identity "CN=User1,CN=Users,DC=corp,DC=contoso,DC=com" -MemberOf "CN=Enterprise Admins,CN=Users,DC=corp,DC=contoso,DC=com","CN=Domain Admins,CN=Users,DC=corp,DC=contoso,DC=com","CN=Schema Admins,CN=Users,DC=corp,DC=contoso,DC=com"
+
+## ログオフ
+## RDP再接続
+## corp\User1 でログオン
