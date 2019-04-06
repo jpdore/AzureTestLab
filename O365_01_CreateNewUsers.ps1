6fe9c826f4a2e38251d183c3017557c37a022c3c
makubass <makubass@gmail.com>
2019-04-06 23:27:13 +0900

    Initial Commit.

---
+++ O365_01_CreateNewUsers.ps1
@@ -0,0 +1,15 @@
+$orgName="dondoko01"
+$loc="JP"
+$licAssignment= $orgName + ":ENTERPRISEPREMIUM"
+
+$userName= "user2@" + $orgName + ".onmicrosoft.com"
+New-MsolUser -DisplayName "User 2" -FirstName User -LastName 2 -UserPrincipalName $userName -UsageLocation $loc -LicenseAssignment $licAssignment
+
+$userName= "user3@" + $orgName + ".onmicrosoft.com"
+New-MsolUser -DisplayName "User 3" -FirstName User -LastName 3 -UserPrincipalName $userName -UsageLocation $loc -LicenseAssignment $licAssignment
+
+$userName= "user4@" + $orgName + ".onmicrosoft.com"
+New-MsolUser -DisplayName "User 4" -FirstName User -LastName 4 -UserPrincipalName $userName -UsageLocation $loc -LicenseAssignment $licAssignment
+
+$userName= "user5@" + $orgName + ".onmicrosoft.com"
+New-MsolUser -DisplayName "User 5" -FirstName User -LastName 5 -UserPrincipalName $userName -UsageLocation $loc -LicenseAssignment $licAssignment
